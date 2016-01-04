00078b9309a111b9a6014b845ad5ae5da6ab531b
bl0ckeduser <bl0ckedusersoft@gmail.com>
2016-01-04 16:11:18 -0500

    Uniformize commenting style

--- diagnostics.c
+++ diagnostics.c
@@ -19,16 +19,20 @@ void compiler_fail(char *message, token_t *token,
 	int chr;
 	int i;
 
-	/* check for specific line/chr override
-	 * (used for special cases like end of line) */
+	/* 
+	 * Check for specific line/chr override
+	 * (used for special cases like end of line)
+	 */
 	line = in_line ? in_line : token->from_line;
 	chr = in_line ? in_chr : token->from_char;
 
 	fflush(stdout);
 	fflush(stderr);
 
-	/* copy the originating code line to a buffer,
-	 * then print this to stderr */
+	/*
+	 * Copy the originating code line to a buffer,
+	 * then print this to stderr 
+	 */
 	strncpy(buf, code_lines[line], 1024);
 	buf[1023] = 0;
 	for (i = 0; code_lines[line]; ++i)
@@ -38,8 +42,10 @@ void compiler_fail(char *message, token_t *token,
 		}
 	fprintf(stderr, "%s\n", buf);
 
-	/* put a little arrow under the offending
-	 * character */
+	/*
+	 * Put a little arrow under the offending
+	 * character
+	 */
 	for (i = 1; i < chr; ++i)
 		if (buf[i - 1] == '\t')
 			fputc(buf[i - 1], stderr);
@@ -48,8 +54,10 @@ void compiler_fail(char *message, token_t *token,
 	fputc('^', stderr);
 	fputc('\n', stderr);
 
-	/* finally display line number and error
-	 * description */
+	/*
+	 * Finally display line number and error
+	 * description
+	 */
 	fprintf(stderr, "line %d: %s\n",
 		line,
 		message);
